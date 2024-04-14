3056f6fc119b0370d803f5bfb4a830df1ed9df8a
wbham <wbham97@naver.com>
2024-04-14 18:17:46 +0900

    [Feat] #1 - DataStore 초기화 및 상세 설정
    - 사용자 정보를 가져오는 두가지 방식. 하나는 특정 시점에 함수로써 가져오는 것. 또 다른 하나는 실시간으로 datastore의 정보를 확인하는 것

---
+++ app/src/main/java/org/shop/yogizogi_android_publish/utils/AppPreference.kt
@@ -0,0 +1,8 @@
+package org.shop.yogizogi_android_publish.utils
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "userPreferences")
