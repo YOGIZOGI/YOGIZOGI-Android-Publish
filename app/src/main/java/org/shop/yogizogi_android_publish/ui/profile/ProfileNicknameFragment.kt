4073ece1114480456be82a24d67810f5056b350c
jonghyuk <harry7408@naver.com>
2024-04-18 16:43:00 +0900

    Feat: profile 제작 tag 요청 화면 구현

--- app/src/main/java/org/shop/yogizogi_android_publish/ui/profile/ProfileNicknameFragment.kt
+++ app/src/main/java/org/shop/yogizogi_android_publish/ui/profile/ProfileNicknameFragment.kt
@@ -1,19 +1,39 @@
 package org.shop.yogizogi_android_publish.ui.profile
 
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.shop.yogizogi_android_publish.databinding.FragmentProfileNicknameBinding
+import org.shop.yogizogi_android_publish.databinding.FragmentProfileTagBinding
 import org.shop.yogizogi_android_publish.ui.base.BaseFragment
 
 @AndroidEntryPoint
-class ProfileNicknameFragment : BaseFragment<FragmentProfileNicknameBinding, ProfileViewModel> (
+class ProfileNicknameFragment : BaseFragment<FragmentProfileNicknameBinding, ProfileViewModel>(
     FragmentProfileNicknameBinding::inflate,
     ProfileViewModel::class.java,
 ) {
     override fun initView() {
-
+        initNextBtn()
     }
 
     override fun initAfterBinding() {
         // TODO
+    }
+
+
+    private fun initNextBtn() {
+        binding.btnNext.setOnClickListener {
+            // TODO : 닉네밍 비었을 때 예외 처리, 닉네임 중복 처리
+
+        }
+    }
+
+    private fun initBackBtn() {
+        // TODO : Fragment 이동
+    }
+
+    private fun navigateToProfileTag() {
+        findNavController().navigate(
+            ProfileNicknameFragmentDirections.actionProfileNicknameFragmentToProfileTagFragment()
+        )
     }
 }
--- app/src/main/java/org/shop/yogizogi_android_publish/ui/profile/ProfileTagFragment.kt
+++ app/src/main/java/org/shop/yogizogi_android_publish/ui/profile/ProfileTagFragment.kt
@@ -1,5 +1,6 @@
 package org.shop.yogizogi_android_publish.ui.profile
 
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.shop.yogizogi_android_publish.databinding.FragmentProfileTagBinding
 import org.shop.yogizogi_android_publish.ui.base.BaseFragment
@@ -10,10 +11,28 @@ class ProfileTagFragment : BaseFragment<FragmentProfileTagBinding, ProfileViewMo
     ProfileViewModel::class.java
 ) {
     override fun initView() {
-
+        initNextButton()
+        initJumpButton()
     }
 
     override fun initAfterBinding() {
 
+    }
+
+    private fun initNextButton() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(
+                ProfileTagFragmentDirections.actionProfileTagFragmentToProfileTasteFragment()
+            )
+        }
+    }
+
+    private fun initJumpButton() {
+        binding.btnJump.setOnClickListener {
+            // TODO 홈 화면으로 이동
+            findNavController().navigate(
+                ProfileTagFragmentDirections.actionProfileTagFragmentToHomeFragment()
+            )
+        }
     }
 }
